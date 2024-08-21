4d3b336e98c4f37fe90948399b3914b494f3258c
Riku Ishibashi <rishibas@c3r6s8.42tokyo.jp>
2024-08-21 15:59:06 +0900

    死ぬ条件のとき哲学者が死ななずに無限ループが発生してしまう問題を解決しました。次は、死なない条件のとき哲学者が死ぬバグを修正する。

--- philo_bonus/handle_philosophers.c
+++ philo_bonus/handle_philosophers.c
@@ -10,7 +10,6 @@ void	waiter(t_philo	*philo)
 void	grim_reaper(t_info *info)
 {
 	int	i;
-	static int count = 0;
 
 	i = 0;
 	while (info->start_time == 0)
@@ -19,20 +18,13 @@ void	grim_reaper(t_info *info)
 	{
 		if (is_philo_dead(&info->philo[i]))
 			break ;
+		i++;
 		if (i == info->num_of_philo)
 			i = 0;
-		i++;
-		// printf("%d\n", count);
-		// count++;
-		// if (count == 1000)
-		// 	break ;
 	}
 	i = -1;
 	while (++i < info->num_of_philo)
-	{
-		printf("---%d : %d\n", i, info->pids[i]);
 		kill(info->pids[i], SIGKILL);
-	}
 }
 
 void	handle_philosophers(t_info *info)
@@ -47,10 +39,7 @@ void	handle_philosophers(t_info *info)
 		if (pid == -1)
 			print_error("failed to fork()");
         else if (pid > 0)
-		{
-			printf("%d : %d\n", i, pid);
 			info->pids[i] = pid;
-		}
 		else if (pid == 0)
 		{
 			waiter(&info->philo[i]);
