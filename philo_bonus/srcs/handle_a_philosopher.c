fedb723f5c7ddcf0774364d63078ef0b0ffcfd58
happyriku <lijiushiqiao@gmail.com>
2024-09-08 11:26:50 +0900

    complete

--- philo_bonus/srcs/handle_a_philosopher.c
+++ philo_bonus/srcs/handle_a_philosopher.c
@@ -14,6 +14,7 @@
 
 void	handle_a_philosopher(t_info *info)
 {
+	info->start_time = get_time();
 	filter_and_output_actions(&info->philo[0], TAKING);
 	skip_time(info->time_to_die);
 	filter_and_output_actions(&info->philo[0], DIE);
